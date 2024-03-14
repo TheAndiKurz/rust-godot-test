416eca3b71af41fed81f6e4bb5c92fe0c7ff8a28
Andreas Kurz <off.andreaskurz+github@gmail.com>
2024-03-14 20:17:48 +0100

    fixed build debug file to build the rust project first

--- build-debug.sh
+++ build-debug.sh
@@ -3,5 +3,9 @@
 # make the target directory
 mkdir -p target/debug
 
+# build the rust library
+cargo build --manifest-path rust/Cargo.toml
+
+
 # export the game
 godot godot/project.godot --export-debug "Linux/X11" ../target/debug/game
